d1577a3caae6ddb6de7d0f8fed9be57a59c8a7c3
mechaadi <as808780@gmail.com>
2022-05-04 14:58:27 +0530

    init

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
     <div className="App">
       <model-viewer
         alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
-        src="/ar/BrainStem.glb"
+        src="/ar/book.gltf"
         ar
         ar-modes="webxr scene-viewer quick-look"
-        height="100%"
         seamless-poster
         shadow-intensity="1"
         camera-controls
         enable-pan
+        style={{ width: "100%", height: "100%" }}
       ></model-viewer>
     </div>
   );
